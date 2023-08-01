a8bc7f3f015e20bf153a8eb2591255074f08da77
yehezkieldio <yehezkieldio35@gmail.com>
2023-08-01 22:55:17 +0800

    initial: let there be light!

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,14 @@
+function App(): JSX.Element {
+    return (
+        <div className="container">
+            <div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione necessitatibus hic excepturi et
+                    ducimus beatae quam nemo eveniet.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
